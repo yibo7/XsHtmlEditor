5131dd6be4d09a57f47b29d360895b132007b99a
yibo7 <cqs263@163.com>
2023-02-27 13:45:54 +0800

    init

---
+++ ClipboardHelper.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XsHtmlEditor { 
+    public static class ClipboardHelper
+    {
+        public static string GetClipboardImageAsBase64()
+        {
+            // 检查剪切板中是否包含图像
+            if (!Clipboard.ContainsImage())
+            {
+                MessageBox.Show("剪板中没有图片");
+                return "";
+                //throw new InvalidOperationException("The clipboard does not contain an image.");
+            }
+
+            // 从剪切板中获取图像对象
+            Image image = Clipboard.GetImage();
+
+            // 将图像转换为字节数组
+            byte[] imageBytes = ImageToBytes(image);
+
+            // 将字节数组转换为 Base64 编码字符串
+            string base64String = Convert.ToBase64String(imageBytes);
+
+            // 返回 Base64 编码字符串
+            return base64String;
+        }
+
+        private static byte[] ImageToBytes(Image image)
+        {
+            // 创建一个内存流对象
+            using (MemoryStream stream = new MemoryStream())
+            {
+                // 将图像以 PNG 格式保存到内存流中
+                image.Save(stream, ImageFormat.Png);
+
+                // 返回内存流中的字节数组
+                return stream.ToArray();
+            }
+        }
+    }
+}
---
+++ HtmlEditor.cs
@@ -0,0 +1,150 @@
+﻿using Microsoft.Web.WebView2.Core;
+using Newtonsoft.Json;
+using NHotkey;
+using NHotkey.WindowsForms;
+using System.Globalization;
+using System.Net.Http.Json;
+using System.Text.Json.Serialization; 
+namespace XsHtmlEditor
+{
+    public class HtmlContent
+    {
+        public HtmlContent(string data) {
+            Data = data;
+        }
+        public string Data { get; set; }
+    }
+    public partial class HtmlEditor : UserControl
+    {
+        private static readonly Keys IncrementKeys = Keys.Shift | Keys.V;
+        public HtmlEditor()
+        {
+            InitializeComponent(); 
+
+            webView2.NavigationCompleted += Wv2Ctrl_NavigationCompleted;
+            webView2.CoreWebView2InitializationCompleted += WebView2_CoreWebView2InitializationCompleted; 
+            webView2.EnsureCoreWebView2Async();
+            HotkeyManager.Current.AddOrReplace("Increment", IncrementKeys, OnIncrement);
+        }
+
+        private void OnIncrement(object? sender, HotkeyEventArgs e)
+        {
+            PasteImgBase64();
+        }
+
+        public Action OnInited;
+
+        //public async Task AwaitInitComplate()
+        //{
+        //    // 等待控件初始化完成
+        //    await webView2.EnsureCoreWebView2Async();
+        //}
+
+        private void WebView2_CoreWebView2InitializationCompleted(object? sender, CoreWebView2InitializationCompletedEventArgs e)
+        {
+            //webView2.CoreWebView2.Navigate("https://www.baidu.com");
+
+            var url = new Uri(@"file:///" + Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"umeditor/xs_index/index.html").Replace('\\', '/'));
+            webView2.Source = url;
+
+            
+
+        }
+
+        /// <summary>
+        /// 获取文本框html
+        /// </summary>
+        /// <returns></returns>
+        async public Task<string> GetHtmlValue()
+        {
+            string script = "getContent()";
+            string content = await webView2.CoreWebView2.ExecuteScriptAsync(script);
+            content = System.Text.RegularExpressions.Regex.Unescape(content);
+            //string escaped = System.Text.RegularExpressions.Regex.Escape(input);
+            return content;
+        }
+
+        /// <summary>
+        /// 设置文本框内容
+        /// </summary>
+        /// <param name="html"></param>
+        async public void SetHtmlValue(string html)
+        {
+
+            HtmlContent data = new HtmlContent(html);
+
+            string content = JsonConvert.SerializeObject(data, Formatting.Indented);
+           
+            string script = $"setContent({content},false)";
+            await webView2.CoreWebView2.ExecuteScriptAsync(script);
+
+        }
+        /// <summary>
+        /// 向光标所在位置插入代码
+        /// </summary>
+        /// <param name="html"></param>
+        async public void InsertHtml(string html)
+        {
+
+            HtmlContent data = new HtmlContent(html);
+
+            string content = JsonConvert.SerializeObject(data, Formatting.Indented);
+
+            string script = $"insertHtml({content})";
+            await webView2.CoreWebView2.ExecuteScriptAsync(script);
+
+        }
+
+        /// <summary>
+        /// 向光标位置粘贴图片
+        /// </summary>
+        /// <param name="html"></param>
+        public void PasteImgBase64()
+        {
+            string base64String = ClipboardHelper.GetClipboardImageAsBase64();
+            if (!string.IsNullOrEmpty(base64String))
+            {
+                InsertHtml($"<img src=\"data:image/png;base64,{base64String}\" alt=\"xs\">");
+            }
+
+        }
+
+
+        /// <summary>
+        /// 获取带有换行格式类的人本
+        /// </summary>
+        /// <returns></returns>
+        async public Task<string> GetTextValue()
+        {
+            string script = "getPlainTxt()";
+            string content = await webView2.CoreWebView2.ExecuteScriptAsync(script);
+            return content;
+        }
+        /// <summary>
+        /// 获取纯文本
+        /// </summary>
+        /// <returns></returns>
+        async public Task<string> GetText()
+        {
+            string script = "getContentTxt()";
+            string content = await webView2.CoreWebView2.ExecuteScriptAsync(script);
+            return content;
+        }
+
+
+        /// <summary>
+        /// 当网页加载完成后触发
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        /// <exception cref="NotImplementedException"></exception>
+        private void Wv2Ctrl_NavigationCompleted(object? sender, CoreWebView2NavigationCompletedEventArgs e)
+        {
+            //SetHtmlValue("");  
+            if (OnInited != null)
+            {
+                OnInited();
+            }
+        }
+    }
+}
